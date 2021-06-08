7c212f48b2e167dac01e9658b0d2ad5e20d321d6
Matthias Gölzner <mgoelzner.mmt-b2019@fh-salzburg.ac.at>
2021-06-08 10:53:51 +0200

    lux fix

--- Plants.a.lot_no_clock/Plants.a.lot_no_clock.ino
+++ Plants.a.lot_no_clock/Plants.a.lot_no_clock.ino
@@ -7,11 +7,13 @@
 
 Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);//lux
 
+#define TSL_SDA 23;
+#define TSL_SCL 19;
+
 // Common naming from FastLED demos
 #define DATA_PIN 17
 #define NUM_LEDS 12//light
 
-#define DHTPIN 18 
 #define AUDIO_PIN 5
 #define SOIL_PIN 34
 
@@ -34,20 +36,6 @@ CRGB leds[NUM_LEDS];
 
 void light (bool x)
 {
-    
-//  EVERY_N_MILLIS(2) 
-//    {
-//      // Clear strip
-//        FastLED.clear();
-//      
-//        // beat8(x) returns a number from 0 to 255, looping x times per minute
-//        uint8_t pos = beat8(bpm); 
-//
-//        // Convert the position to an index for the LED on our strip
-//        uint8_t led = map8(pos,0,NUM_LEDS-1);
-//        leds[led] = CRGB::Green;
-//        FastLED.show();
-//    }
   FastLED.clear();
   if (x == true) {
     for(int led=0; led<NUM_LEDS; led++)
@@ -60,28 +48,28 @@ void light (bool x)
   FastLED.show();
 }
 
-//lux
-void configureSensor(void)
-{
+void configureLux() {
+  Serial.println("wut?");
+  if(tsl.begin()) {
+    Serial.println("tsl found");
+  } else {
+    Serial.println("nope");
+  }
   tsl.setGain(TSL2591_GAIN_MED);      // 25x gain
   tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
   tsl2591Gain_t gain = tsl.getGain();
 }
 
-void simpleRead(void)
-{
-  uint16_t x = tsl.getLuminosity(TSL2591_VISIBLE);
-}
-
 void setup() 
 { 
-   Serial.begin(9600);
-   dht.begin();
-   //soil
-   pinMode(SOIL_PIN, INPUT);
-
+  Serial.begin(9600);
+  Wire.begin(TSL_SDA, TSL_SCL);
+  delay(100);
+  dht.begin();
+  //soil
+  pinMode(SOIL_PIN, INPUT);
   //lux
-  configureSensor();
+  configureLux();
    
    FastLED.setBrightness(80);
    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
@@ -89,30 +77,29 @@ void setup()
 }
 
 void loop() {
-  
   EVERY_N_MILLIS(700) 
-   {
-    simpleRead();//lux
-    if (tsl.getLuminosity(TSL2591_VISIBLE)< 1100)
+  {
+    uint16_t x = tsl.getLuminosity(TSL2591_VISIBLE);
+    if (x< 1100)
     {
-      light (true);
+      light(true);
       Serial.print("Lux: ");
-      Serial.println(tsl.getLuminosity(TSL2591_VISIBLE));
+      Serial.println(x);
       Serial.println(" -----------------");
     }
 
     else
     {
-      light (false);
-      Serial.println("OK");
+      light(false);
+      Serial.println("Light OK");
     }
-   }
+  }
   
-   EVERY_N_MILLIS(1100) 
-   {
-   hum = dht.readHumidity();
-   temp= dht.readTemperature();
-
+  EVERY_N_MILLIS(1100) 
+  {
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+  
     if (hum < 40)
     {
       light (true);
@@ -128,11 +115,12 @@ void loop() {
     else
     {
       light(false);
+      Serial.println("Hum OK");
     }
-   }
+  }
    
-   EVERY_N_MILLIS(1300) 
-   {
+  EVERY_N_MILLIS(1300) 
+  {
     int sensorValue = analogRead(SOIL_PIN);
     if (analogRead(SOIL_PIN)>2500)
     {
@@ -144,7 +132,7 @@ void loop() {
     else
     {
       light(false);
+      Serial.println("Soil OK");
     }
-   }
-  
+  }
 }
